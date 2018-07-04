9ba83321b0368bcc4883f34a923df7a882c0ee76
Rudy Sanchez <rasanchez@mpsaz.org>
2018-07-04 08:06:01 -0700

    Initial setup. vuex, vue-router, etc.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
+import { store } from './store'
 
 Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
